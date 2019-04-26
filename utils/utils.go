e06c9ae4aac28b46e2af6dea6f57a90fc29c5513
Tillo <tillo.bosshart@gmail.com>
2019-04-26 14:09:12 +0200

    StringInSlices refactored

--- utils/utils.go
+++ utils/utils.go
@@ -47,3 +47,12 @@ func CleanUpDir(dst string, del []string) (err error) {
 	}
 	return
 }
+
+func StringInSlice(s string, list []string) bool {
+	for _, b := range list {
+		if b == s {
+			return true
+		}
+	}
+	return false
+}
