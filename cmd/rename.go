8bf82b5898e44ec6c434fcd1578e1dcf63fa4cf1
Tillo Bosshart <tillo.bosshar@gmail.com>
2019-11-14 16:54:29 +0100

    rename implementation

--- cmd/rename.go
+++ cmd/rename.go
@@ -16,17 +16,65 @@ limitations under the License.
 package cmd
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/morgulbrut/colorlog"
 	"github.com/spf13/cobra"
 )
 
 // renameCmd represents the rename command
 var renameCmd = &cobra.Command{
-	Use:   "rename",
+	Use:   "rename <oldname> <newname>",
 	Short: "Renames a project and it's files",
 	Run: func(cmd *cobra.Command, args []string) {
-		colorlog.Info("rename called")
+		if len(args) == 2 {
+			oldname := args[0]
+			newname := args[1]
+			renameFiles(oldname, newname)
+		} else {
+			colorlog.Fatal("Wrong amount of arguments")
+			os.Exit(0)
+		}
 	},
+}
+
+func renameFiles(oldname, newname string) {
+	colorlog.Info("Renaming project %s to %s", oldname, newname)
+	files, err := filepath.Glob("*" + oldname + "*")
+	if err != nil {
+		colorlog.Fatal(err.Error())
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		colorlog.Fatal(err.Error())
+	}
+
+	for _, f := range files {
+		colorlog.Debug("renaming %s", f)
+		oldpath := filepath.Join(pwd, f)
+		filetype := strings.Split(f, ".")[1]
+		newpath := filepath.Join(pwd, newname+"."+filetype)
+		os.Rename(oldpath, newpath)
+	}
+}
+
+func fixProject(oldname, newname string) {
+	in, err := ioutil.ReadFile(newname + ".PrjPCB")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	input := string(in)
+	out := []byte(strings.ReplaceAll(input, "DocumentPath="+oldname, "DocumentPath="+newname))
+
+	err = ioutil.WriteFile(newname+".PrjPCB", out, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func init() {
