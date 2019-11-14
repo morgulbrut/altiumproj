dbb56044319a7ba103d8d4b602fec254faa565ac
Tillo Bosshart <tillo.bosshar@gmail.com>
2019-11-14 14:17:04 +0100

    Added logo for more swag

--- main.go
+++ main.go
@@ -14,8 +14,23 @@
 
 package main
 
-import "github.com/morgulbrut/altiumproj/cmd"
+import (
+	"github.com/morgulbrut/altiumproj/cmd"
+	"github.com/morgulbrut/color256"
+)
 
 func main() {
+	logo()
 	cmd.Execute()
+}
+
+func logo() {
+	color256.Init()
+	color256.PrintRandom(" █████╗")
+	color256.PrintRandom("██╔══██╗██╗  ████████╗██╗██╗   ██╗███╗   ███╗██████╗ ██████╗  ██████╗      ██╗")
+	color256.PrintRandom("██║  ██║██║  ╚══██╔══╝██║██║   ██║████╗ ████║██╔══██╗██╔══██╗██╔═══██╗     ██║")
+	color256.PrintRandom("███████║██║     ██║   ██║██║   ██║██╔████╔██║██████╔╝██████╔╝██║   ██║     ██║")
+	color256.PrintRandom("██╔══██║██║     ██║   ██║██║   ██║██║╚██╔╝██║██╔═══╝ ██╔══██╗██║   ██║██   ██║")
+	color256.PrintRandom("██║  ██║███████╗██║   ██║╚██████╔╝██║ ╚═╝ ██║██║     ██║  ██║╚██████╔╝╚█████╔╝")
+	color256.PrintRandom("╚═╝  ╚═╝╚══════╝╚═╝   ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ")
 }
